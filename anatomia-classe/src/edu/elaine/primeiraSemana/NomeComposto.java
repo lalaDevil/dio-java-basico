0dc9a50108cd12f1d3ff9d27583b708e4b05098b
lalaDevil <elainemg14@gmail.com>
2024-07-03 15:27:14 -0300

    atualização

---
+++ anatomia-classe/src/edu/elaine/primeiraSemana/NomeComposto.java
@@ -0,0 +1,15 @@
+package edu.elaine.primeiraSemana;
+
+public class NomeComposto {
+    public static void main(String[] args) {
+        String primeiroNome = "Gleyson";
+      String segundoNome = "Sampaio";
+      String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+      System.out.println(nomeCompleto);
+   }
+
+   public static String nomeCompleto(String primeiroNome, String segundoNome) {
+      return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
+   }
+    
+}
