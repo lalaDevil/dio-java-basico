81b54bfa3719917dbc5f16a5557dfb293b56326e
lalaDevil <elainemg14@gmail.com>
2024-07-23 23:45:47 -0300

    meu projeto do desafio

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -12,19 +12,19 @@ public class ContaTerminal {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US); //cria o novo objeto scanner
         //nome do cliente
         System.out.println("Olá, seja bem vindo!\nPor favor digite seu nome e sobrenome: ");
-        String nome = scanner.nextLine();
+        String nomeCliente = scanner.nextLine();
         //numero da agencia
         System.out.println("Por favor, digite o número da Agência: ");
         String agencia = scanner.nextLine();
         //numero da conta
         System.out.println("Digite o número da conta: ");
-        int conta = scanner.nextInt();
+        int numero = scanner.nextInt();
         //valor do saldo
         System.out.println("Por favor, digite o valor do saldo: ");
         double saldo = scanner.nextDouble();
 
         //Impressão dos dados obtidos pelo usuário
-        System.out.println("Olá "+nome+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+conta+" e seu saldo de "+saldo+" já está disponível para saque");
+        System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo de "+saldo+" já está disponível para saque");
 
     }
 }
