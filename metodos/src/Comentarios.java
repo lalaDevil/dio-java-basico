50317960628946f899467dd176e221558cbcbb00
lalaDevil <elainemg14@gmail.com>
2024-07-23 22:25:48 -0300

    att

---
+++ metodos/src/Comentarios.java
@@ -0,0 +1,22 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // COmentários em uma única linha
+
+        /*
+         * ola
+         * comentario mais detalhado
+         */
+
+    }
+    /**
+     * duas estrelinhas acima identifica
+     * a elaboração de um comentário
+     * a nível de documentação.
+     * ao passar o mouse acima do método 
+     * abaixo é possível visualizar
+     */
+    public void metodo(){
+
+    }
+    
+}
